61a4d2973f623563a7b999997194128bc9a4bfce
Mark Silvis <masilvis22@gmail.com>
2016-04-06 19:41:16 -0400

    Removes unused code

--- src/main/java/edu/pitt/dbmi/ccd/db/service/AnnotationService.java
+++ src/main/java/edu/pitt/dbmi/ccd/db/service/AnnotationService.java
@@ -45,48 +45,12 @@ public class AnnotationService {
     @Autowired
     private AnnotationRepository annotationRepository;
 
-//    @Autowired(required=true)
-//    public AnnotationService(AnnotationRepository annotationRepository) {
-//        this.annotationRepository = annotationRepository;
-//    }
-
-//    public Annotation update(Annotation annotation, String accessName, String groupName) {
-//        final Access access = accessService.findByName(accessName);
-//        final Group group = (groupName != null)
-//                          ? groupService.findByName(groupName)
-//                          : null;
-//        // no changes, don't update
-//        if (annotation.getAccess().getName().equalsIgnoreCase(access.getName()) && annotation.getGroup().getName().equalsIgnoreCase(group.getName())) {
-//            return annotation;
-//        // update just group
-//        } else if (annotation.getAccess().getName().equalsIgnoreCase(access.getName()) && access.getName().equalsIgnoreCase("GROUP") && !annotation.getGroup().getName().equalsIgnoreCase(group.getName())) {
-//            annotation.setGroup(group);
-//            return save(annotation);
-//        // update access from private to group and update group
-//        } else if (annotation.getAccess().getName().equalsIgnoreCase("PRIVATE") && access.getName().equalsIgnoreCase("GROUP")) {
-//            if (group == null) {
-//              throw new AccessUpdateException(true);
-//            } else {
-//              annotation.setAccess(access);
-//              annotation.setGroup(group);
-//              return save(annotation);
-//            }
-//        // update access from private to public
-//        } else if (annotation.getAccess().getName().equalsIgnoreCase("PRIVATE") && access.getName().equalsIgnoreCase("PUBLIC")) {
-//            annotation.setAccess(access);
-//            return save(annotation);
-//        // update access from group to public and remove group
-//        } else if (annotation.getAccess().getName().equalsIgnoreCase("GROUP") && access.getName().equalsIgnoreCase("PUBLIC")) {
-//            annotation.setAccess(access);
-//            annotation.setGroup(null);
-//            return save(annotation);
-//        } else {
-//            throw new AccessUpdateException(annotation.getAccess(), access);
-//        }
-//    }
-
     public Annotation save(Annotation annotation) {
         return annotationRepository.save(annotation);
+    }
+
+    public Annotation saveAndFlush(Annotation annotation) {
+        return annotationRepository.saveAndFlush(annotation);
     }
 
     public Optional<Annotation> findById(UserAccount requester, Long id) {
