7cacb1ab176c00d6a9fc15a7c2a6b968c7da7b3e
Kevin Bui <kvb2univpitt@gmail.com>
2016-08-03 16:35:10 -0400

    Added basic services for annotation repositories.

---
+++ src/main/java/edu/pitt/dbmi/ccd/db/service/OauthRefreshTokenService.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2016 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package edu.pitt.dbmi.ccd.db.service;
+
+import edu.pitt.dbmi.ccd.db.repository.OauthRefreshTokenRepository;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * Aug 3, 2016 4:29:01 PM
+ *
+ * @author Mark Silvis (marksilvis@pitt.edu)
+ */
+@Service
+@Transactional
+public class OauthRefreshTokenService {
+
+    private final OauthRefreshTokenRepository oauthRefreshTokenRepository;
+
+    @Autowired
+    public OauthRefreshTokenService(OauthRefreshTokenRepository oauthRefreshTokenRepository) {
+        this.oauthRefreshTokenRepository = oauthRefreshTokenRepository;
+    }
+
+}
