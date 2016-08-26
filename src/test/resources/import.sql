5720b7f3ab5861ec140257a3432b33357f13a43e
Mark Silvis <marksilvis@pitt.edu>
2016-08-26 12:03:30 -0400

    Includes adjustments to test database sql file to make compatible with new version

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,8 +1,8 @@
 -- author: Mark Silvis
 -- Test data
 
--- Create Accesses
-INSERT INTO access (id, name, description) VALUES (1, 'PUBLIC','Visable to all users'), (2, 'GROUP', 'Visible to members of specified group'), (3, 'PRIVATE','Visable only to creator');
+-- Create Share Accesses
+INSERT INTO share_access (id, name, description) VALUES (1, 'PUBLIC','Visable to all users'), (2, 'GROUP', 'Visible to members of specified group'), (3, 'PRIVATE','Visable only to creator');
 
 -- Create Persons
 INSERT INTO person (id, description, email, first_name, last_name, middle_name, workspace) VALUES (1, 'Physicist', 'isaac@example.com', 'Isaac', 'Newton', NULL, '~/ccd_workspace/'), (2, 'Computer Scientist', 'alan@example.com', 'Alan', 'Turing', NULL, '~/ccd_workspace/');
@@ -19,26 +19,26 @@ INSERT INTO user_role (id, name, description) VALUES (1, 'USER', 'Standard user'
 -- Create User Accounts
 INSERT INTO user_account (id, person_id, user_login_id, user_login_attempt_id, user_role_id, username, password, active, disabled, registration_date, registration_location, account, activation_key) VALUES (1, 1, 1, 1, 2, 'isaac', '$2a$10$LTFYDfz3oBc96cKgaOkz2OIy8OYlT5o5TTYaJCRXr0s2.ahXAuAvK', b'1', b'0', CURRENT_DATE, 323223552, 'f0e4d573-e85a-4c52-8426-89fa55f36c7c', 'abcde'), (2, 2, 2, 2, 2, 'alan', '$2a$10$ad0/roYTf7qhQlAjXl5m5.duYGtMw4dHgN.bauA/mi5eZV67937WS', b'1', b'0', CURRENT_DATE, 323223552, '2a7dc1f4-a95c-42e1-a694-792acdc2e2c5', 'vwxyz');
 
--- Create Groups
-INSERT INTO groups (id, name, description) VALUES (1, 'Scientists', 'Group of famous scientists');
+-- Create Share Groups
+INSERT INTO share_group (id, name, description, owner) VALUES (1, 'Scientists', 'Group of famous scientists', 1);
 
--- Create Group_Membership mappings
-INSERT INTO group_membership VALUES (1, 1);     -- User Isaac is a member
+-- Create Share Group Membership mappings
+INSERT INTO share_group_membership VALUES (1, 1);     -- User Isaac is a member
 
--- Create Group_Moderation mappings
-INSERT INTO group_moderation VALUES (1, 1);     -- User Isaac is a moderator
+-- Create Share Group Requests mappings
+INSERT INTO shaer_group_requests VALUES (1, 2);       -- User Alan is requesting Group access
 
--- Create Group_Requests mappings
-INSERT INTO group_requests VALUES (1, 2);       -- User Alan is requesting Group access
+-- Create Address for Annotation Target
+INSERT INTO address (id, title, url, user_account_id) VALUES (1, 'Department of Biomedical Informatics', 'http://dbmi.pitt.edu', 1);
 
 -- Create Annotation Target
-INSERT INTO annotation_target (id, title, created, modified, version, address, user_account_id) VALUES (1, 'Department of Biomedical Informatics', '2016-03-24 15:58:15', NULL, 0, 'http://dbmi.pitt.edu', 1);
+INSERT INTO annotation_target (id, created_date, modified_date, modify_count, address, user_account_id) VALUES (1, '2016-03-24 15:58:15', NULL, 0, 1, 1);
 
 -- Create Vocabularies
-INSERT INTO vocabulary (id, description, name, version) VALUES (1, 'Text with no required structure', 'Plaintext', 0);
+INSERT INTO vocabulary (id, description, name, created_date, version) VALUES (1, 'Text with no required structure', 'Plaintext', '2016-03-24 15:58:15', '20150514');
 
 -- Create Attributes
-INSERT INTO attribute (id, level, name, requirement_level, vocab_id) VALUES (1, NULL, 'text', NULL, 1);
+INSERT INTO attribute (id, name, required, vocab_id) VALUES (1, 'text', 1, 1);
 
 -- Create Annotations with AnnotationData
 -- Public
